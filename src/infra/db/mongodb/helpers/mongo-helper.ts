d6fd8fe46c785e1cdcb78e526e4c9d76d70e3f60
Lucas Marques <lucas.msilva09@gmail.com>
2021-03-05 16:58:44 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,5 +1,4 @@
-import { connect , MongoClient } from 'mongodb'
-import { disconnect } from 'process'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -13,5 +12,9 @@ export const MongoHelper = {
 
   async disconnect (): Promise<void> {
     await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
