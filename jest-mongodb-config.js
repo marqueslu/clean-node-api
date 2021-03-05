f9ab833950d608bf0d6bb30a70f7d28fb81d83f0
Lucas Marques <lucas.msilva09@gmail.com>
2021-03-05 16:58:04 -0300

    chore: update jest mongodb config

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,12 +1,10 @@
 module.exports = {
   mongodbMemoryServerOptions: {
     binary: {
-      version: '4.0.3',
+      version: '4.4.1',
       skipMD5: true
     },
-    instance: {
-      dbName: 'jest'
-    },
+    instance: {},
     autoStart: false
   }
 }
